d0617a8bea72be32575b27791a48aa2b0fa607ed
Thiago T. Moura <rpj134@gmail.com>
2022-06-19 13:38:47 -0300

    showing the wrong guesses

--- main.cpp
+++ main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
 #include <string>
 #include <map>
-
+#include <vector>
 #include "main.hpp"
 
 using namespace std;
 
 const string SECRET_WORD = "MELANCIA";
 map<char, bool> tried;
+vector<char> wrong_guesses;
 
 int main()
 {
+    cout << "***************************************\n";
+    cout << "***   Welcome to the Hangman Game   ***\n";
+    cout << "***************************************\n\n";
+
     bool hanged = false;
     bool win = false;
 
     while(!hanged && !win)
     {
+        cout << "Wrong guesses: ";
+        for(char letter : wrong_guesses)
+        {
+            cout << letter << " ";
+        }
+        cout << endl;
+
+
         for(char letter : SECRET_WORD)
         {
             if(tried[letter])
@@ -24,21 +37,24 @@ int main()
             }else
             {
                 cout << "_ ";
-            }
-            
+            }            
         }
+        cout << endl;
 
         char guess;
+
+        cout << "Guess one letter: ";
         cin >> guess;
 
         tried[guess] = true;
 
         if(have_letter(guess))
         {
-            cout << "The letter " << guess << " was in the secret word\n";
+            cout << "The letter " << guess << " was in the secret word\n\n";
         }else
         {
-            cout << "The letter " << guess << " wasn't in the secret word\n";
+            cout << "The letter " << guess << " wasn't in the secret word\n\n";
+            wrong_guesses.push_back(guess);
         }
     }
 }
