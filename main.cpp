7cb2512f9577003228e2807492ef511e00c26ae2
Thiago T. Moura <rpj134@gmail.com>
2022-06-18 22:35:14 -0300

    receiving guesses

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    
+    string secret_word = "MELANCIA";
+    cout << secret_word << endl;
+
+    bool hanged = false;
+    bool win = false;
+
+    while(!hanged && !win)
+    {
+        char guess;
+        cin >> guess;
+
+        cout << "Your guess was " << guess << endl;
+        
+    }
+
 }
