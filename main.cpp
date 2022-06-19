dab4a3361f997b6e12829fd719c62fa0d3be2c05
Thiago T. Moura <rpj134@gmail.com>
2022-06-19 13:11:46 -0300

    verify if the guess was in the secret word

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
+
+#include "main.hpp"
+
 using namespace std;
+
+const string SECRET_WORD = "MELANCIA";
 
 int main()
 {
-    string secret_word = "MELANCIA";
-    cout << secret_word << endl;
+    cout << SECRET_WORD << endl;
 
     bool hanged = false;
     bool win = false;
@@ -15,8 +19,24 @@ int main()
         char guess;
         cin >> guess;
 
-        cout << "Your guess was " << guess << endl;
-        
+        if(have_letter(guess))
+        {
+            cout << "The letter " << guess << " was in the secret word\n";
+        }else
+        {
+            cout << "The letter " << guess << " wasn't in the secret word\n";
+        }
     }
+}
 
+bool have_letter(char guess)
+{
+    for(char letter : SECRET_WORD)
+    {
+        if(guess == letter)
+        {
+            return true;
+        }
+    }
+    return false;
 }
---
+++ main.hpp
@@ -0,0 +1,1 @@
+bool have_letter(char guess);
